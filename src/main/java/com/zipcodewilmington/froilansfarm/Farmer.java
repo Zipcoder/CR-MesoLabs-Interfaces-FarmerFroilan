98f2f676ad3d13a78eadbb6ec8ab05239e1a27df
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-01 12:41:03 -0500

    All classes and interfaces added, next is methods

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farmer.java
@@ -0,0 +1,9 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Farmer extends Person implements Eater, Rider, Botanist{
+
+    public void plant(Crop cropToPlant, CropRow rowToPlantIn) {
+
+    }
+
+}
