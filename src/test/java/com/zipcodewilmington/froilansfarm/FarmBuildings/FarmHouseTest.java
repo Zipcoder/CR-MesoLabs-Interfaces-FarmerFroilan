5941be32b03eacccd3ab9449c63b7d24809c59fa
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-02 14:55:54 -0500

    quick field test

--- src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/FarmHouseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/FarmHouseTest.java
@@ -1,4 +1,26 @@
 package com.zipcodewilmington.froilansfarm.FarmBuildings;
 
+import com.zipcodewilmington.froilansfarm.Person.Farmer;
+import com.zipcodewilmington.froilansfarm.Person.Person;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class FarmHouseTest {
+
+    FarmHouse testFarmHouse;
+
+    @Before
+    public void setUp(){
+        testFarmHouse = new FarmHouse();
+    }
+
+    @Test
+    public void testFarmHouse(){
+        Farmer testPerson = new Farmer();
+        testFarmHouse.getPeople().add(testPerson);
+        int actual = testFarmHouse.getPeople().size();
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/FieldTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/FieldTest.java
@@ -1,4 +1,27 @@
 package com.zipcodewilmington.froilansfarm.FarmBuildings;
 
+import com.zipcodewilmington.froilansfarm.Crops.CropRow;
+import com.zipcodewilmington.froilansfarm.Edibles.Tomato;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class FieldTest {
+    Field testField;
+
+    @Before
+    public void setUp(){
+        testField = new Field();
+    }
+    @Test
+    public void testField(){
+        CropRow testRow = new CropRow();
+        Tomato testTomato = new Tomato();
+        testRow.getCropRow().add(testTomato);
+        testField.getCropRow()[0] = testRow;
+        String actual = testField.getCropRow().getClass().getSimpleName();
+        String expected = "CropRow[]";
+        Assert.assertEquals(expected, actual);
+
+    }
 }
