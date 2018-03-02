be70a96cdb27643c3ba3356644495166bf671d49
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-02 18:19:00 -0500

    saving

--- src/test/java/com/zipcodewilmington/froilansfarm/farm/things/vehicles/CropDusterTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/farm/things/vehicles/CropDusterTest.java
@@ -1,4 +1,33 @@
 package com.zipcodewilmington.froilansfarm.farm.things.vehicles;
 
+import com.zipcodewilmington.froilansfarm.farm.things.livingthings.creatures.people.Pilot;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CropDusterTest {
+
+    CropDuster badLarry;
+    Pilot froilanda;
+
+    @Before
+    public void setUp(){
+        badLarry = new CropDuster("Bad Larry");
+        froilanda = new Pilot("Froilanda");
+    }
+
+
+    @Test
+    public void flyTest(){
+        //Given
+
+        //When
+        froilanda.mount(badLarry);
+        badLarry.fly();
+
+        //Then
+        Assert.assertTrue(badLarry.isFlying());
+    }
+
+
 }
