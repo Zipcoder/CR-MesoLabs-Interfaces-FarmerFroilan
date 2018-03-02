77a885b844c614990c9de3cb3f8bd725ab075c57
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-02 14:36:30 -0500

    chickencoop and stable

--- src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/ChickenCoopTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/ChickenCoopTest.java
@@ -1,6 +1,10 @@
 package com.zipcodewilmington.froilansfarm.FarmBuildings;
 
+import com.zipcodewilmington.froilansfarm.Animals.Chicken;
+import com.zipcodewilmington.froilansfarm.Edibles.Tomato;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ChickenCoopTest {
 
@@ -10,4 +14,14 @@ public class ChickenCoopTest {
     public void setUp(){
         testCoop = new ChickenCoop();
     }
+
+    @Test
+    public void testCoopChicks(){
+        Chicken testChick = new Chicken();
+        testCoop.getChickens().add(testChick);
+        int actual = testCoop.getChickens().size();
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
+
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/StableTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmBuildings/StableTest.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.froilansfarm.FarmBuildings;
 
+import com.zipcodewilmington.froilansfarm.Animals.Horse;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StableTest {
+
+    Stable testStable;
+
+    @Before
+    public void setUp(){
+        testStable = new Stable();
+    }
+
+    @Test
+    public void testStable(){
+        Horse testHorse = new Horse();
+        testStable.getHorses().add(testHorse);
+        int actual = testStable.getHorses().size();
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 }
