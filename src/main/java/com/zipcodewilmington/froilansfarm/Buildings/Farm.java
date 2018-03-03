5fa5fabfa4b55e1ea2577884491270fc9affa658
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-03 15:47:37 -0500

    ha

--- src/main/java/com/zipcodewilmington/froilansfarm/Buildings/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Buildings/Farm.java
@@ -3,8 +3,15 @@ package com.zipcodewilmington.froilansfarm.Buildings;
 import com.zipcodewilmington.froilansfarm.Crops.Crop;
 import com.zipcodewilmington.froilansfarm.Crops.CropRow;
 import com.zipcodewilmington.froilansfarm.Crops.Field;
+import com.zipcodewilmington.froilansfarm.FarmAnimal.Chicken;
+import com.zipcodewilmington.froilansfarm.FarmAnimal.Horse;
+import com.zipcodewilmington.froilansfarm.Interfaces.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.Person.Farmer;
 import com.zipcodewilmington.froilansfarm.Person.Person;
+import com.zipcodewilmington.froilansfarm.Person.Pilot;
+import com.zipcodewilmington.froilansfarm.Vehicles.Aircraft;
+import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
+import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
 
 import java.util.ArrayList;
 
@@ -13,7 +20,23 @@ public class Farm {
     public Farm() {
 
         Person froilan = new Farmer();
+        Person froilanda = new Pilot();
+
         Field field = new Field();
+
+        ChickenCoop chickenCoop1 = new ChickenCoop();
+        ChickenCoop chickenCoop2 = new ChickenCoop();
+        ChickenCoop chickenCoop3 = new ChickenCoop();
+        ChickenCoop chickenCoop4 = new ChickenCoop();
+
+        Stable stable1 = new Stable();
+        Stable stable2 = new Stable();
+        Stable stable3 = new Stable();
+
+        FarmVehicle tractor = new Tractor();
+        FarmVehicle tractor2 = new Tractor();
+
+        Aircraft cropDuster = new CropDuster();
 
         CropRow rowOfCornStalk = new CropRow();
         CropRow rowOfCornStalk2 = new CropRow();
@@ -21,11 +44,43 @@ public class Farm {
         CropRow rowOfTomatoPlants = new CropRow();
         CropRow rowOfBananaTrees = new CropRow();
 
-        //field.addRowsOfCrops();
+        field.addRowsOfCrops(rowOfCornStalk);
+        field.addRowsOfCrops(rowOfCornStalk2);
+        field.addRowsOfCrops(rowOfCornStalk3);
+        field.addRowsOfCrops(rowOfTomatoPlants);
+        field.addRowsOfCrops(rowOfBananaTrees);
+
+        chickenCoop1.addChicken(new Chicken());
+        chickenCoop1.addChicken(new Chicken());
+        chickenCoop1.addChicken(new Chicken());
+        chickenCoop1.addChicken(new Chicken());
+        chickenCoop1.addChicken(new Chicken());
+
+        chickenCoop2.addChicken(new Chicken());
+        chickenCoop2.addChicken(new Chicken());
+        chickenCoop2.addChicken(new Chicken());
+
+        chickenCoop3.addChicken(new Chicken());
+        chickenCoop3.addChicken(new Chicken());
+        chickenCoop3.addChicken(new Chicken());
+
+        chickenCoop4.addChicken(new Chicken());
+        chickenCoop4.addChicken(new Chicken());
+        chickenCoop4.addChicken(new Chicken());
+        chickenCoop4.addChicken(new Chicken());
+
+        stable1.addHorse(new Horse());
+        stable1.addHorse(new Horse());
+        stable1.addHorse(new Horse());
+
+        stable2.addHorse(new Horse());
+        stable2.addHorse(new Horse());
+        stable2.addHorse(new Horse());
+
+        stable3.addHorse(new Horse());
+        stable3.addHorse(new Horse());
+        stable3.addHorse(new Horse());
+        stable3.addHorse(new Horse());
+
     }
-
-    ArrayList<Stable> stables = new ArrayList<>();
-    ArrayList<ChickenCoop> chickenCoops = new ArrayList<>();
-    FarmHouse farmHouse = new FarmHouse();
-
 }
