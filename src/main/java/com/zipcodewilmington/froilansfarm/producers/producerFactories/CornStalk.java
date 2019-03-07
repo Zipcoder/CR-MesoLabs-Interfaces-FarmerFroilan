43efa66a77537e4bb6c485856fc350968ec41162
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-07 17:28:13 -0500

    Started cornstalk

--- src/main/java/com/zipcodewilmington/froilansfarm/producers/producerFactories/CornStalk.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/producers/producerFactories/CornStalk.java
@@ -1,6 +1,21 @@
 package com.zipcodewilmington.froilansfarm.producers.producerFactories;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.zipcodewilmington.froilansfarm.producers.produce.Corn;
 import com.zipcodewilmington.froilansfarm.producers.produce.Crop;
+import com.zipcodewilmington.froilansfarm.producers.produce.ProduceInterface;
 
 public class CornStalk extends Crop {
+    private Boolean isFertilized;
+
+    public CornStalk(){}
+
+    public CornStalk(Boolean isFertilized){}
+
+    @Override
+    public Corn yield(){
+        return null;
+    }
+
+
 }
