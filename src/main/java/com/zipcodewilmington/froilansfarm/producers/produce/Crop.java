926a66b2171cbb99efabcef39efb434ee6efa58f
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-03-07 14:54:04 -0500

    UML approved changes

--- src/main/java/com/zipcodewilmington/froilansfarm/producers/produce/Crop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/producers/produce/Crop.java
@@ -2,7 +2,7 @@ package com.zipcodewilmington.froilansfarm.producers.produce;
 
 import com.zipcodewilmington.froilansfarm.interfaceBehaviors.Edible;
 
-public class Crop implements ProduceInterface {
+public abstract class  Crop implements ProduceInterface {
     public Edible yield() {
         return null;
     }
--- src/main/java/com/zipcodewilmington/froilansfarm/producers/producerFactories/CropRows.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/producers/producerFactories/CropRows.java
@@ -1,7 +1,12 @@
 package com.zipcodewilmington.froilansfarm.producers.producerFactories;
 
 import com.zipcodewilmington.froilansfarm.producers.produce.Crop;
+import com.zipcodewilmington.froilansfarm.storage.Storage;
 
-public class CropRows {
+public class CropRows implements Storage {
     private Crop crops;
+
+    public void add(Object storageObject) {
+
+    }
 }
