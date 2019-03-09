552bf7e3148357cbefa1de044a0f3846d0c410b6
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-09 13:19:35 -0500

    Field done

--- src/test/java/com/zipcodewilmington/froilansfarm/CropTests/FieldTests.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/CropTests/FieldTests.java
@@ -1,4 +1,66 @@
 package com.zipcodewilmington.froilansfarm.CropTests;
 
+import com.zipcodewilmington.froilansfarm.storage.CropRows;
+import com.zipcodewilmington.froilansfarm.storage.Field;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FieldTests {
+    @Test
+    public void nullaryConstructorTest(){
+        // given
+        Field field = new Field();
+        Integer expected = 0;
+        // when
+        Integer actual = field.getNumberOfCropRows();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTest(){
+        // given
+        Field field = new Field(5);
+        Integer expected = 5;
+        // when
+        Integer actual = field.getNumberOfCropRows();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOfCropRowsTest(){
+        // given
+        Field field = new Field(4);
+        Integer expected = 4;
+        // when
+        Integer actual = field.getNumberOfCropRows();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNumberOfCropRowsTest(){
+        // given
+        Field field = new Field();
+        Integer expected = 7;
+        // when
+        field.setNumberOfCropRows(7);
+        Integer actual = field.getNumberOfCropRows();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addTest(){
+        // given
+        Field field = new Field();
+        CropRows cropRows = new CropRows();
+        Integer expected = 1;
+        // when
+        field.add(cropRows);
+        Integer actual = field.getNumberOfCropRows();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
 }
