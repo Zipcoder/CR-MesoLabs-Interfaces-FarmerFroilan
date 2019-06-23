5549a53a73ad8c015e36e45cb04884426f98ebd5
Reese Watson <sherrese.watson@gmail.com>
2019-06-23 15:36:16 -0400

    update

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animal/ChickenTest.java
@@ -0,0 +1,40 @@
+package com.zipcodewilmington.froilansfarm.Animal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChickenTest {
+
+    @Test
+    public void fertilized() {
+    }
+
+    @Test
+    public void yieldEggs() {
+    }
+
+    @Test
+    public void isHungry() {
+    }
+
+    @Test
+    public void reproduce() {
+    }
+
+    @Test
+    public void eatEdible() {
+    }
+
+    @Test
+    public void hasEaten() {
+    }
+
+    @Test
+    public void getFoodEaten() {
+    }
+
+    @Test
+    public void makeNoise() {
+    }
+}
