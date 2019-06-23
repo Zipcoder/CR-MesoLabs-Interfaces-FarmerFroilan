797ac765f270cb6356da69e1eb14be91e3f05c0d
prasanthipenmadu <prasanthipl@gmail.com>
2019-06-23 10:20:26 -0400

    Committing Second time

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animal/FarmerTest.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.froilansfarm.Animal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FarmerTest {
+
+    @Test
+    public void farmerHarvest() {
+    }
+
+    @Test
+    public void foodStore() {
+    }
+}
