9f5d45af0c5d73850f54d330a473b5b1124275c2
Reese Watson <sherrese.watson@gmail.com>
2019-06-23 18:51:30 -0400

    update

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Pilot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Pilot.java
@@ -2,7 +2,7 @@ package com.zipcodewilmington.froilansfarm.Animal;
 
 import com.zipcodewilmington.froilansfarm.Vehicles.Flyable;
 
-public interface Pilot {
+public interface Pilot extends Person {
 
         public void flyAirCraft(Flyable airCraft);
         public void ceaseFlying(Flyable airCraft);
--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Rider.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Rider.java
@@ -3,7 +3,7 @@ package com.zipcodewilmington.froilansfarm.Animal;
 import com.zipcodewilmington.froilansfarm.Vehicles.Rideable;
 
 
-public interface Rider {
+public interface Rider extends Person{
 
         public void mount(Rideable rideable);
         public void dismount(Rideable rideable);
