18f3e7efb8bad384e4e5089b6718e85fad42e908
Stefun <hawkd215@gmail.com>
2019-06-25 14:00:43 -0400

    Garage updated

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Garage.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Garage.java
@@ -3,35 +3,67 @@ package com.zipcodewilmington.froilansfarm.Shelter;
 import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
 import com.zipcodewilmington.froilansfarm.Vehicles.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
+import com.zipcodewilmington.froilansfarm.Vehicles.Vehicle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Garage implements Shelter {
-    List<FarmVehicle>  farmVehicles=new ArrayList<FarmVehicle>();
-    Tractor tractor1=new Tractor();
-    Tractor tractor2=new Tractor();
-    CropDuster cropDuster=new CropDuster();
-    public int total(){
-        farmVehicles.add(tractor1);
-        farmVehicles.add(tractor2);
-        farmVehicles.add(cropDuster);
-        return farmVehicles.size();
+    List<Tractor> tractors = new ArrayList<>();
+    List<CropDuster> cropDusters = new ArrayList<>();
+
+
+    public Garage() {
     }
 
     public Boolean isEmpty() {
-
-        return farmVehicles.isEmpty();
+        if (tractors.isEmpty() && cropDusters.isEmpty()){
+            return true;
+        }
+        return false;
     }
 
+    public void add(Tractor tractor) {
+        tractors.add(tractor);
+    }
+
+    public void remove(Tractor tractor) {
+        tractors.remove(tractor);
+    }
+
+    public Integer tractorCount(){
+        return tractors.size();
+    }
+
+    public void add(CropDuster cropDuster) {
+        cropDusters.add(cropDuster);
+    }
+
+    public void remove(CropDuster cropDuster) {
+        cropDusters.remove(cropDuster);
+    }
+
+    public Integer cropDusterCount(){
+        return cropDusters.size();
+    }
+
+    public Integer totalItemCount() {
+
+        return tractorCount() + cropDusterCount();
+    }
+
+    @Override
     public void add() {
 
     }
 
+    @Override
     public void remove() {
+
 
     }
 
+    @Override
     public Integer itemCount() {
         return null;
     }
