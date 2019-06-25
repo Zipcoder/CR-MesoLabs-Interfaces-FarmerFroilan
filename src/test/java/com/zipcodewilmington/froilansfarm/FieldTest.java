70327792c0f26e3a6c07f1372a417da0470d1033
Stefun <hawkd215@gmail.com>
2019-06-25 18:17:31 -0400

    Garage and Field Tests complete

--- src/test/java/com/zipcodewilmington/froilansfarm/FieldTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FieldTest.java
@@ -1,132 +1,131 @@
-//package com.zipcodewilmington.froilansfarm;
-//
-//import com.zipcodewilmington.froilansfarm.Crops.CropRow;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-//public class FieldTest {
-//
-//    @Test
-//    public void testIsEmpty1() {
-//        Field field = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//
-//        field.add(cropRow1);
-//
-//        Boolean expected = false;
-//        Boolean actual = field.isEmpty();
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void TestIsEmpty2() {
-//        Field field = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//
-//        Boolean expected = true;
-//        Boolean actual = field.isEmpty();
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//
-//    @Test
-//    public void testAdd1() {
-//        Field field = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//
-//        field.add(cropRow1);
-//        String expected = "cropRow1";
-//        String actual = field.toString();
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void testAdd2() {
-//        Field field = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//        CropRow cropRow2 = new CropRow<>();
-//
-//        field.add(cropRow1);
-//        field.add(cropRow2);
-//
-//        Boolean actual = field.isEmpty();
-//
-//        assertFalse(actual);
-//    }
-//
-//    @Test
-//    public void testRemove1() {
-//        Field field1 = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//        CropRow cropRow2 = new CropRow<>();
-//
-//        field1.add(cropRow1);
-//        field1.add(cropRow2);
-//
-//        field1.remove(cropRow1);
-//
-//        String expected = "cropRow2";
-//        String actual = field1.toString();
-//
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void testRemove2() {
-//        Field field1 = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//        CropRow cropRow2 = new CropRow<>();
-//
-//        field1.add(cropRow1);
-//        field1.add(cropRow2);
-//
-//        field1.remove(cropRow1);
-//        field1.remove(cropRow2);
-//
-//        Boolean expected = true;
-//        Boolean actual = field1.isEmpty();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void TestItemCount1() {
-//        Field field1 = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//        CropRow cropRow2 = new CropRow<>();
-//
-//        field1.add(cropRow1);
-//        field1.add(cropRow2);
-//
-//        Integer expected = 2;
-//        Integer actual = field1.itemCount();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    public void TestItemCount2() {
-//        Field field1 = new Field();
-//        CropRow cropRow1 = new CropRow<>();
-//        CropRow cropRow2 = new CropRow<>();
-//
-//        field1.add(cropRow1);
-//        field1.add(cropRow2);
-//
-//        field1.remove(cropRow1);
-//        field1.remove(cropRow2);
-//
-//        Integer expected = null;
-//        Integer actual = field1.itemCount();
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
-//}
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.Crops.CropRow;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+
+    @Test
+    public void testIsEmpty1() {
+        Field field = new Field();
+        CropRow cropRow1 = new CropRow<>();
+
+        field.add(cropRow1);
+
+        Boolean expected = false;
+        Boolean actual = field.isEmpty();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestIsEmpty2() {
+        Field field = new Field();
+        CropRow cropRow1 = new CropRow<>();
+
+        Boolean expected = true;
+        Boolean actual = field.isEmpty();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testAdd1() {
+        Field field = new Field();
+        CropRow cropRow1 = new CropRow<>();
+
+        field.add(cropRow1);
+        String expected = "cropRow1";
+        String actual = field.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAdd2() {
+        Field field = new Field();
+        CropRow cropRow1 = new CropRow<>();
+        CropRow cropRow2 = new CropRow<>();
+
+        field.add(cropRow1);
+        field.add(cropRow2);
+
+        Boolean actual = field.isEmpty();
+
+        assertFalse(actual);
+    }
+
+    @Test
+    public void testRemove1() {
+        Field field1 = new Field();
+        CropRow cropRow1 = new CropRow<>();
+        CropRow cropRow2 = new CropRow<>();
+
+        field1.add(cropRow1);
+        field1.add(cropRow2);
+
+        field1.remove(cropRow1);
+
+        String expected = "cropRow2";
+        String actual = field1.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemove2() {
+        Field field1 = new Field();
+        CropRow cropRow1 = new CropRow<>();
+        CropRow cropRow2 = new CropRow<>();
+
+        field1.add(cropRow1);
+        field1.add(cropRow2);
+
+        field1.remove(cropRow1);
+        field1.remove(cropRow2);
+
+        Boolean expected = true;
+        Boolean actual = field1.isEmpty();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestItemCount1() {
+        Field field1 = new Field();
+        CropRow cropRow1 = new CropRow<>();
+        CropRow cropRow2 = new CropRow<>();
+
+        field1.add(cropRow1);
+        field1.add(cropRow2);
+
+        Integer expected = 2;
+        Integer actual = field1.itemCount();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestItemCount2() {
+        Field field1 = new Field();
+        CropRow cropRow1 = new CropRow<>();
+        CropRow cropRow2 = new CropRow<>();
+
+        field1.add(cropRow1);
+        field1.add(cropRow2);
+
+        field1.remove(cropRow1);
+        field1.remove(cropRow2);
+
+        Integer expected = null;
+        Integer actual = field1.itemCount();
+
+        assertEquals(expected, actual);
+    }
+}
+
--- src/test/java/com/zipcodewilmington/froilansfarm/Shelter/GarageTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Shelter/GarageTest.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.froilansfarm.Shelter;
 
+import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
+import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,26 +9,246 @@ import static org.junit.Assert.*;
 public class GarageTest {
 
     @Test
-    public void total() {
+    public void testIsEmpty1() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+
+        garage1.add(tractor1);
+
+        Boolean expected = false;
+        Boolean actual = garage1.isEmpty();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void isEmpty() {
+    public void TestIsEmpty2() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+
+        Boolean expected = true;
+        Boolean actual = garage1.isEmpty();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void atCapacity() {
+    public void testIsEmpty3() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+
+        garage1.add(cropDuster1);
+
+        Boolean expected = false;
+        Boolean actual = garage1.isEmpty();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void add() {
+    public void TestIsEmpty4() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+
+        Boolean expected = true;
+        Boolean actual = garage1.isEmpty();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testAdd1() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+
+        garage1.add(tractor1);
+        String expected = "tractor1";
+        String actual = garage1.toString();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void remove() {
+    public void testAdd2() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+        Tractor tractor2 = new Tractor();
+
+        garage1.add(tractor1);
+        garage1.add(tractor2);
+
+        Boolean actual = garage1.isEmpty();
+
+        assertFalse(actual);
     }
 
     @Test
-    public void itemCount() {
+    public void testAdd3() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+
+        garage1.add(cropDuster1);
+        String expected = "cropDuster1";
+        String actual = garage1.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAdd4() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+        CropDuster cropDuster2 = new CropDuster();
+
+        garage1.add(cropDuster1);
+        garage1.add(cropDuster2);
+
+        Boolean actual = garage1.isEmpty();
+
+        assertFalse(actual);
+    }
+
+
+    @Test
+    public void testRemove1() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+        Tractor tractor2 = new Tractor();
+
+        garage1.add(tractor1);
+        garage1.add(tractor2);
+
+        garage1.remove(tractor1);
+
+        String expected = "tractor2";
+        String actual = garage1.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemove2() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+        Tractor tractor2 = new Tractor();
+
+        garage1.add(tractor1);
+        garage1.add(tractor2);
+
+        garage1.remove(tractor1);
+        garage1.remove(tractor2);
+
+        Boolean expected = true;
+        Boolean actual = garage1.isEmpty();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemove3() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+        CropDuster cropDuster2 = new CropDuster();
+
+        garage1.add(cropDuster1);
+        garage1.add(cropDuster2);
+
+        garage1.remove(cropDuster1);
+
+        String expected = "cropDuster2";
+        String actual = garage1.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemove4() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+        CropDuster cropDuster2 = new CropDuster();
+
+        garage1.add(cropDuster1);
+        garage1.add(cropDuster2);
+
+        garage1.remove(cropDuster1);
+        garage1.remove(cropDuster2);
+
+        Boolean expected = true;
+        Boolean actual = garage1.isEmpty();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestItemCount1() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+        Tractor tractor2 = new Tractor();
+
+        garage1.add(tractor1);
+        garage1.add(tractor2);
+
+        Integer expected = 2;
+        Integer actual = garage1.itemCount();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestItemCount2() {
+        Garage garage1 = new Garage();
+        Tractor tractor1 = new Tractor();
+        Tractor tractor2 = new Tractor();
+
+        garage1.add(tractor1);
+        garage1.add(tractor2);
+
+        garage1.remove(tractor1);
+        garage1.remove(tractor2);
+
+        Integer expected = null;
+        Integer actual = garage1.itemCount();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestItemCount3() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+        CropDuster cropDuster2 = new CropDuster();
+
+        garage1.add(cropDuster1);
+        garage1.add(cropDuster2);
+
+        Integer expected = 2;
+        Integer actual = garage1.itemCount();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void TestItemCount4() {
+        Garage garage1 = new Garage();
+        CropDuster cropDuster1 = new CropDuster();
+        CropDuster cropDuster2 = new CropDuster();
+
+        garage1.add(cropDuster1);
+        garage1.add(cropDuster2);
+
+        garage1.remove(cropDuster1);
+        garage1.remove(cropDuster2);
+
+        Integer expected = null;
+        Integer actual = garage1.itemCount();
+
+        assertEquals(expected, actual);
+
     }
 }
