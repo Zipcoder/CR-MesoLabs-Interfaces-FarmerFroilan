3bf97c36ecd03ef5f6b8bf574ea8198517aa6f3d
MikeGray31 <michaelkgray31@gmail.com>
2019-11-19 17:17:23 -0500

    Update

--- src/main/java/com/zipcodewilmington/froilansfarm/crops/WatermelonPlants.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/WatermelonPlants.java
@@ -4,6 +4,9 @@ import com.zipcodewilmington.froilansfarm.edibles.Edibles;
 import com.zipcodewilmington.froilansfarm.edibles.Watermelon;
 import com.zipcodewilmington.froilansfarm.interfaces.Produces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WatermelonPlants implements Produces {
 
     private Boolean hasBeenFertilized;
@@ -12,10 +15,17 @@ public class WatermelonPlants implements Produces {
         this.hasBeenFertilized = false;
     }
 
-    public Edibles yield(){
+    public List<Watermelon> yield(){
         if(hasBeenFertilized){
-            Watermelon watermelon = new Watermelon();
-            return watermelon;
+
+            List<Watermelon> yieldGroup = new ArrayList<Watermelon>();
+            int yieldAmount = (int)(Math.random() * ((4 - 2) + 2)) + 1;
+
+            for(int i = 0; i < yieldAmount; i++){
+                Watermelon watermelon = new Watermelon();
+                yieldGroup.add(watermelon);
+            }
+            return yieldGroup;
         }
         return null;
     }
