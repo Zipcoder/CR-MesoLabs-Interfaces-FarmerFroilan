8fdcd7e10cbb6801c6caeac9959883ccbfd992c9
mike <mike.schoenbeck@gmail.com>
2019-11-19 16:26:18 -0500

    added most methods for all person types

--- src/main/java/com/zipcodewilmington/froilansfarm/people/Pilot.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/people/Pilot.java
@@ -1,4 +1,28 @@
 package com.zipcodewilmington.froilansfarm.people;
 
-public class Pilot {
+import com.zipcodewilmington.froilansfarm.interfaces.Rideable;
+import com.zipcodewilmington.froilansfarm.vehicles.Aircraft;
+
+public class Pilot extends Person {
+    Aircraft aircraft;
+
+
+    Pilot(String name) {
+        super(name);
+    }
+
+    public void mount(Rideable rideable)    {
+        this.aircraft.fly();
+
+    }
+
+
+    public void disMount(Rideable rideable) {   //TODO sets aircraft's current status
+
+    }
+
+
+
+
+
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/people/Rider.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/people/Rider.java
@@ -1,4 +1,22 @@
 package com.zipcodewilmington.froilansfarm.people;
 
-public class Rider {
+import com.zipcodewilmington.froilansfarm.interfaces.Rideable;
+
+public class Rider extends Farmer{
+
+    private String name;
+
+
+    Rider(String name) {
+        super(name);
+    }
+
+    public void mount(Rideable rideable)    {   //TODO sets farm equipment's current status
+
+    }
+
+
+    public void disMount(Rideable rideable) {   //TODO sets farm equipment's current status
+
+    }
 }
