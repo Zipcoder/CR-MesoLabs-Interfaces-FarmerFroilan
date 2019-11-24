30fe0505ede25c6c04146fa0fc7b29aa03a1b66a
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-24 11:49:26 -0500

    chicken coop

--- src/main/java/com/zipcodewilmington/froilansfarm/mainFarm/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/mainFarm/ChickenCoop.java
@@ -3,17 +3,35 @@ package com.zipcodewilmington.froilansfarm.mainFarm;
 import com.zipcodewilmington.froilansfarm.animals.Chicken;
 import com.zipcodewilmington.froilansfarm.edibles.Eggs;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
 
 public class ChickenCoop {
     private Chicken chicken = new Chicken();
-    public Chicken[] chickenCoop = {chicken,chicken,chicken,chicken,chicken,chicken,chicken,chicken,chicken,chicken};
+
+    private Chicken[] chickenCoop1 = {chicken,chicken,chicken};
+    private Chicken[] chickenCoop2 = {chicken,chicken,chicken,chicken};
+    private Chicken[] chickenCoop3 = {chicken,chicken,chicken,chicken};
+    private Chicken[] chickenCoop4 = {chicken,chicken,chicken,chicken};
+    private ArrayList<Chicken[]> chickenCoops;
     public ArrayList<Eggs> eggStorage;
 
+    public void populateChickenCoops(){
+        chickenCoops.add(chickenCoop1);
+        chickenCoops.add(chickenCoop2);
+        chickenCoops.add(chickenCoop3);
+        chickenCoops.add(chickenCoop4);
+    }
+
     public void dailyEggs(){
-        for(Chicken c : chickenCoop){
-            Eggs egg = c.layEgg();
-            eggStorage.add(egg);
+        for(Chicken[] cc : chickenCoops){
+            for(Chicken c : cc){
+                Eggs egg = c.layEgg();
+                eggStorage.add(egg);
+            }
+
         }
     }
 }
