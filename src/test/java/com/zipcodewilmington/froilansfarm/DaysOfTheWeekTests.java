54d1fa8a1339b37f146148467698f51ab8fd8a92
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-24 12:25:21 -0500

    tests

--- src/test/java/com/zipcodewilmington/froilansfarm/DaysOfTheWeekTests.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/DaysOfTheWeekTests.java
@@ -13,18 +13,14 @@ import org.junit.Test;
 
 public class DaysOfTheWeekTests {
 
-    Froilan froilan = new Froilan("Froilan");
-    Froilanda froilanda = new Froilanda("Froilanda");
-    Horse horse1 = new Horse();
-    Horse horse2 = new Horse();
-    CropDuster cropDuster = new CropDuster();
-    Field field = new Field();
-    Tractor tractor = new Tractor();
-    TomatoPlants tomatoes;
-
-    @Before
-    public void setUpFarm() {
-    }
+    private Froilan froilan = new Froilan("Froilan");
+    private Froilanda froilanda = new Froilanda("Froilanda");
+    private Horse horse1 = new Horse();
+    private Horse horse2 = new Horse();
+    private CropDuster cropDuster = new CropDuster();
+    private Field field = new Field();
+    private Tractor tractor = new Tractor();
+    private TomatoPlants tomatoes;
 
 
 
@@ -79,21 +75,57 @@ public class DaysOfTheWeekTests {
 
     @Test
     public void WednesdayTest(){
+        froilan.mount(horse1);
+        froilanda.mount(horse2);
+        Assert.assertTrue(horse1.mounted());
+        Assert.assertTrue(horse2.mounted());
+
+        froilan.dismount(horse1);
+        froilanda.dismount(horse2);
+        Assert.assertFalse(horse1.dismounted());
+        Assert.assertFalse(horse2.dismounted());
 
     }
 
     @Test
     public void ThursdayTest(){
+        froilan.mount(horse1);
+        froilanda.mount(horse2);
+        Assert.assertTrue(horse1.mounted());
+        Assert.assertTrue(horse2.mounted());
+
+        froilan.dismount(horse1);
+        froilanda.dismount(horse2);
+        Assert.assertFalse(horse1.dismounted());
+        Assert.assertFalse(horse2.dismounted());
 
     }
 
     @Test
     public void FridayTest(){
+        froilan.mount(horse1);
+        froilanda.mount(horse2);
+        Assert.assertTrue(horse1.mounted());
+        Assert.assertTrue(horse2.mounted());
+
+        froilan.dismount(horse1);
+        froilanda.dismount(horse2);
+        Assert.assertFalse(horse1.dismounted());
+        Assert.assertFalse(horse2.dismounted());
 
     }
 
     @Test
     public void SaturdayTest(){
+        froilan.mount(horse1);
+        froilanda.mount(horse2);
+        Assert.assertTrue(horse1.mounted());
+        Assert.assertTrue(horse2.mounted());
+
+        froilan.dismount(horse1);
+        froilanda.dismount(horse2);
+        Assert.assertFalse(horse1.dismounted());
+        Assert.assertFalse(horse2.dismounted());
 
     }
 }
