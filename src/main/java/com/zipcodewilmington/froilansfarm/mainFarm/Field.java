77e8e6be36ea28a2fc5e18f40bc4e82fb0eab5af
MikeGray31 <michaelkgray31@gmail.com>
2019-11-27 10:14:55 -0500

    Added CropRow class

--- src/main/java/com/zipcodewilmington/froilansfarm/mainFarm/Field.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/mainFarm/Field.java
@@ -2,15 +2,27 @@ package com.zipcodewilmington.froilansfarm.mainFarm;
 
 import com.zipcodewilmington.froilansfarm.crops.Crop;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Field {
+public class Field<E extends Crop> {
 
-    private List<CropRow> cropRowContents;
+    private List<CropRow> fieldRows;
 
-    public void addRow(Crop cropType, int howManyPlants){
+    public Field(){
+        fieldRows = new ArrayList<CropRow>();
+    }
 
+    public void addRow(E cropType, int howManyPlants){
+        fieldRows.add(new CropRow(cropType,howManyPlants));
     }
 
 
+    public List<CropRow> getFieldRows() {
+        return fieldRows;
+    }
+
+    public void setFieldRows(List<CropRow> fieldRows) {
+        this.fieldRows = fieldRows;
+    }
 }
