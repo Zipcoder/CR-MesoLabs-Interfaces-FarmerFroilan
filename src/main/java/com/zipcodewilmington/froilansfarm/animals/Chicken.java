26d538892dedb1c2661cf6fd83da4d56f6d27d2a
Mo Russ <mauriceruss302@gmail.com>
2020-03-03 16:56:18 -0500

    Chicken class started instance variables declared, stubs for tests made

--- src/main/java/com/zipcodewilmington/froilansfarm/animals/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/animals/Chicken.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.froilansfarm.animals;
 
-public class Chicken {
+import com.zipcodewilmington.froilansfarm.food.Edible;
+
+import java.util.ArrayList;
+
+public class Chicken implements Animal{
+    private Integer id;
+    private ArrayList<String> edible;
+
+    public Chicken(Integer id, ArrayList edible){
+        this.id = id;
+        this.edible = edible;
+    }
+
+    
+
+    public boolean eatFood(Edible edible) {
+        return false;
+    }
+
+    public String makeNoise() {
+        return null;
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/animalstest/ChickenTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/animalstest/ChickenTest.java
@@ -1,4 +1,21 @@
 package com.zipcodewilmington.froilansfarm.animalstest;
 
+import org.junit.Test;
+
 public class ChickenTest {
+    @Test
+    public void eatFoodTest(){
+        //
+
+    }
+
+    @Test
+    public void makeNoiseTest(){
+
+    }
+
+    @Test
+    public void chickenConstructorTest(){
+
+    }
 }
