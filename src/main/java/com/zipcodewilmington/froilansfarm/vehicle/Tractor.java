5f149007538893ce1645c907b522b783308d9cad
Chris Farmer <farmerc@udel.edu>
2020-03-03 16:52:35 -0500

    almost finished vehicles

--- src/main/java/com/zipcodewilmington/froilansfarm/vehicle/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/vehicle/Tractor.java
@@ -1,4 +1,33 @@
 package com.zipcodewilmington.froilansfarm.vehicle;
 
-public class Tractor {
+import com.zipcodewilmington.froilansfarm.farm.CropRow;
+import com.zipcodewilmington.froilansfarm.person.Farmer;
+import com.zipcodewilmington.froilansfarm.person.Rider;
+
+import java.util.logging.Logger;
+
+public class Tractor extends Vehicle implements FarmVehicle{
+
+    private static final Logger LOGGER = Logger.getLogger(Tractor.class.getName());
+
+    private Rider rider;
+
+    public Tractor(Rider rider){
+        this.rider = rider;
+    }
+
+    public void operate(CropRow row) {
+        if (!(rider instanceof Farmer)){
+            //rider.crash();
+        }
+    }
+
+    public void crash(){
+        LOGGER.info("Oh no " + /*rider.getName()*/ " has crashed and died!");
+        //rider.die();
+    }
+
+    public void MakeNoise() {
+        System.out.println("Track Track Track Track...");
+    }
 }
