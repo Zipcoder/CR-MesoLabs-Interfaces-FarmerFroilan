0f2913f50b301bcfdc5046ddad22df23fc30b551
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-04 08:18:22 -0500

    adding getters and setters

--- src/test/java/com/zipcodewilmington/froilansfarm/peopletest/FarmerTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/peopletest/FarmerTest.java
@@ -1,18 +1,21 @@
 package com.zipcodewilmington.froilansfarm.peopletest;
 
+import com.zipcodewilmington.froilansfarm.food.Egg;
 import com.zipcodewilmington.froilansfarm.people.Farmer;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class FarmerTest
 {
     @Test
-    public void TestFarmer()
+    public void TestEggHarvest()
     {
         Farmer farmer1 = new Farmer();
+        ArrayList<Egg> eggNum = new ArrayList<Egg>();
+        Assert.assertFalse(eggNum.size() == 0);
 
-        Boolean outcome = farmer1.equals(true);
-
-        Assert.assertTrue(outcome);
     }
 }
