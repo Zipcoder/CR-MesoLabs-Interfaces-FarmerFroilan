f4ef345008aefd4705a4da4cbcd7bffae89ada7a
Mo Russ <mauriceruss302@gmail.com>
2020-03-03 19:05:05 -0500

    Horse class started setIdTest passed

--- src/main/java/com/zipcodewilmington/froilansfarm/animals/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/animals/Horse.java
@@ -1,4 +1,38 @@
 package com.zipcodewilmington.froilansfarm.animals;
 
-public class Horse {
+import com.zipcodewilmington.froilansfarm.food.Edible;
+import com.zipcodewilmington.froilansfarm.vehicles.Vehicle;
+
+public class Horse extends Vehicle implements Animal {
+    private Integer id;
+
+    public Horse(Integer id){
+        this.id = id;
+    }
+    public Horse() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean eatFood(Edible edible) {
+        return true;
+    }
+
+    public String makeNoise() {
+        return null;
+    }
+
+    public boolean ride() {
+        return false;
+    }
+
+    public boolean stopRiding() {
+        return false;
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/animalstest/HorseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/animalstest/HorseTest.java
@@ -1,4 +1,35 @@
 package com.zipcodewilmington.froilansfarm.animalstest;
 
+import com.zipcodewilmington.froilansfarm.animals.Chicken;
+import com.zipcodewilmington.froilansfarm.animals.Horse;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HorseTest {
+
+
+    @Test
+    public void setId() {
+        Horse horse1 = new Horse();
+        Integer expected = 10;
+        horse1.setId(expected);
+        Integer actual = horse1.getId();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void eatFood() {
+    }
+
+    @Test
+    public void makeNoise() {
+    }
+
+    @Test
+    public void ride() {
+    }
+
+    @Test
+    public void stopRiding() {
+    }
 }
