f0e4328f8b2d1f7a6e4c2dcd849ae968cfbd715d
Mo Russ <mauriceruss302@gmail.com>
2020-03-03 19:07:28 -0500

    eatFoodTest passed

--- src/test/java/com/zipcodewilmington/froilansfarm/animalstest/HorseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/animalstest/HorseTest.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.froilansfarm.animalstest;
 
 import com.zipcodewilmington.froilansfarm.animals.Chicken;
 import com.zipcodewilmington.froilansfarm.animals.Horse;
+import com.zipcodewilmington.froilansfarm.food.Edible;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,7 +10,7 @@ public class HorseTest {
 
 
     @Test
-    public void setId() {
+    public void setIdTest() {
         Horse horse1 = new Horse();
         Integer expected = 10;
         horse1.setId(expected);
@@ -18,18 +19,25 @@ public class HorseTest {
     }
 
     @Test
-    public void eatFood() {
+    public void eatFoodTest() {
+        Horse horse1 = new Horse();
+        Edible earCorn = new Edible() {};
+        boolean result;
+        if (horse1.eatFood(earCorn)) result = true;
+        else result = false;
+        Boolean answer = true;
+        Assert.assertTrue("Let's eat", true);
     }
 
     @Test
-    public void makeNoise() {
+    public void makeNoiseTest() {
     }
 
     @Test
-    public void ride() {
+    public void rideTest() {
     }
 
     @Test
-    public void stopRiding() {
+    public void stopRidingTest() {
     }
 }
