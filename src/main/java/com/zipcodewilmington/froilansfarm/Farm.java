044d68dcd8c6b25d7e2b703dcfb3efb374701efc
vle <obentoka@gmail.com>
2020-03-04 19:44:48 -0500

    updated farm

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,12 +1,34 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.animals.Chicken;
+import com.zipcodewilmington.froilansfarm.animals.Horse;
+import com.zipcodewilmington.froilansfarm.farmland.CropRow;
 import com.zipcodewilmington.froilansfarm.farmland.Field;
+import com.zipcodewilmington.froilansfarm.storage.ChickenCoop;
+import com.zipcodewilmington.froilansfarm.storage.Stable;
 
 public class Farm {
     public Field field;
+    public ChickenCoop coop1;
+    public ChickenCoop coop2;
+    public ChickenCoop coop3;
+    public ChickenCoop coop4;
+    public Stable stable1;
+    public Stable stable2;
+    public Stable stable3;
 
     public Farm() {
         this.field = new Field();
+        for (int i = 0; i < 5; i++) {
+            field.addCropRowsToCropField(new CropRow());
+            coop1.add(new Chicken());
+            coop2.add(new Chicken());
+            coop3.add(new Chicken());
+        }
+        for (int i = 0; i < 3; i++) {
+            stable1.add(new Horse());
+        }
+
     }
 
     public Field getField() {
--- src/main/java/com/zipcodewilmington/froilansfarm/weekdays/DailyRoutines.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/weekdays/DailyRoutines.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.froilansfarm.weekdays;
 
+import com.zipcodewilmington.froilansfarm.people.Person;
+
 public abstract class DailyRoutines {
+
+    public Boolean morningRoutine(){
+
+        return null;
+    }
+
+    public Boolean dailyRoutine(){
+        return null;
+    }
+
+    public Boolean eveningRoutine(){
+        return null;
+    }
+
+    public Boolean rideHorse(Person man, Person woman){
+
+
+        return null;
+    }
 }
