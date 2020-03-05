e05392193e7507b3d69f2c97f42da3fc4d65ee63
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-03-04 19:04:18 -0500

    started testing

--- src/test/java/com/zipcodewilmington/froilansfarm/MainApplicationTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/MainApplicationTest.java
@@ -1,6 +1,11 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.sun.javafx.stage.StageHelper;
+import com.zipcodewilmington.froilansfarm.crop.*;
+import com.zipcodewilmington.froilansfarm.farm.CropRow;
 import com.zipcodewilmington.froilansfarm.farm.Farm;
+import com.zipcodewilmington.froilansfarm.farm.Field;
+import com.zipcodewilmington.froilansfarm.food.Tomato;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,8 +13,14 @@ import org.junit.Test;
  * Created by leon on 2/26/18.
  */
 public class MainApplicationTest {
-    Farm theFarm = new Farm(3,4,1);
-
+//    Farm theFarm = new Farm(3,4,1);
+//    Field theField = new Field();
+//    theField.setCropRow(1, CornStalk);
+//    theField.setCropRow(2,TomatoPlant);
+//    theField.setCropRow(3,OnionRoot);
+//    theField.setCropRow(4,PotatoRoot);
+//    theField.setCropRow(5,CarrotRoot);
+//
 //    @Test
 //    public void testFarmConstructor(){
 //        //number of Stables
@@ -24,9 +35,22 @@ public class MainApplicationTest {
 //        int expected = 4;
 //        int actual = theFarm.getNumOfFields();
 //    }
-
+//
 //    @Test
-//    public void
+//    public void testSetCropRow(){
+//        // cornStalk
+//        CropRow expected = new CornStalk();
+//        CropRow actual = theField.getCropRow(1);
+//        //TomatoPlant
+//        theField.getCropRow(2);
+//        //OnionRoot
+//        theField.getCropRow(3);
+//        //PotatoRoot
+//        theField.getCropRow(4);
+//        //CarrotRoot
+//        theField.getCropRow(5);
+//
+//    }
 
 
 }
