e6a9d313dfb8f53894ca040fea29df2e837f4129
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-28 17:35:01 -0400

    half the main done

--- src/main/java/com/zipcodewilmington/froilansfarm/shelter/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/shelter/Farm.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.froilansfarm.shelter;
 
+import com.zipcodewilmington.froilansfarm.person.Farmer;
+import com.zipcodewilmington.froilansfarm.person.Person;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +13,26 @@ public class Farm {
     FarmHouse farmHouse = new FarmHouse();
 
     //Possibly instantiate vehicles/people/animals here? -> LEAVING THIS UP TO YOU ABE lol
+    //Yeah, that's fair. I'm the one using em anyhow, lol. --Abe
 
     public Farm(){
-
     }
 
+    public List<ChickenCoop> getChickenCoops() {
+        return chickenCoops;
+    }
+    public List<Stables> getStables(){return stables;}
 
+    public void addStable(Stables s){
+        stables.add(s);
+    }
+    public void addCoop(ChickenCoop c){
+        chickenCoops.add(c);
+    }
+    public FarmHouse getHouse(){
+        return farmHouse;
+    }
+    public void moveIn(Farmer wack){
+        farmHouse.add(wack);
+    }
 }
